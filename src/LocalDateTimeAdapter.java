c03265669735efc32db3142b559801e9c7076717
ksaNNN <kalinichenko_716@mail.ru>
2026-09-15 09:53:06 +0300

    Разделение GsonAdapters на отдельные файлы по codeStyle

--- src/LocalDateTimeAdapter.java
+++ src/LocalDateTimeAdapter.java
@@ -4,31 +4,10 @@ import com.google.gson.stream.JsonToken;
 import com.google.gson.stream.JsonWriter;
 
 import java.io.IOException;
-import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-class DurationAdapter extends TypeAdapter<Duration> {
-    @Override
-    public void write(JsonWriter out, Duration value) throws IOException {
-        if (value == null) {
-            out.nullValue();
-        } else {
-            out.value(value.toMinutes());
-        }
-    }
-
-    @Override
-    public Duration read(JsonReader in) throws IOException {
-        if (in.peek() == JsonToken.NULL) {
-            in.nextNull();
-            return null;
-        }
-        return Duration.ofMinutes(in.nextLong());
-    }
-}
-
-class LocalDateTimeAdapter extends TypeAdapter<LocalDateTime> {
+public class LocalDateTimeAdapter extends TypeAdapter<LocalDateTime> {
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
 
     @Override
